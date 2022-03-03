20becc72a1865bd69b56af2f8611668a7a7b1773
dzuerst <dzuerst@gmail.com>
2022-03-03 10:51:27 +0700

    perubahan nama file

---
+++ fundamental/01arrays.kt
@@ -0,0 +1,20 @@
+fun main(){
+    // Pendeklarasian array dasar
+    val myArray = arrayOf(1,2,3,4);
+
+    // Array dengan nilai berbagai tipe data
+    val mixArray = arrayOf(1,2,"halo",true);
+    /*
+        // array dengan elemen-elemennya memiliki tipe data yang sama
+        
+        // intArrayOf() : IntArray
+        // booleanArrayOf() : BooleanArray
+        // charArrayOf() : CharArray
+        // longArrayOf() : LongArray
+        // shortArrayOf() : ShortArray
+        // byteArrayOf() : ByteArray
+    */
+
+    // mengakses elemen array
+    print(mixArray[2]);
+}
