227adc8718b78a5eeb658bd64f2116bb3e4914e4
dzuerst <dzuerst@gmail.com>
2022-03-03 11:59:59 +0700

    pengenalan control flow

---
+++ 02control-flow/01pengenalan.kt
@@ -0,0 +1,12 @@
+fun main(){
+    // Control flow adalah cara kita mengontrol 
+    // alur dari sebuah program berdasarkan kondisi saat program tersebut berjalan.
+
+//     Terdapat beberapa bagian dari control flow, antara lain:
+        // Enumeration
+        // When Expression
+        // Expression & Statement
+        // While and Do While
+        // Range and For Loop
+        // Break and Continue Labels
+}
