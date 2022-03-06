9206aff96babd68198e5706217173af38ed0ea67
dzuerst <dzuerst@gmail.com>
2022-03-06 09:19:50 +0700

    while and do while

---
+++ 02control-flow/04whiledowhile.kt
@@ -0,0 +1,19 @@
+fun main(){
+    var counter =1
+    // while
+    // cek baru lakukan
+    while (counter <= 7){
+        println("Hello, World")
+        counter++
+    }
+
+    println()
+
+    //do while
+    // lakukan baru cek
+    var num = 1;
+    do {
+        println("Hello Brader")
+        num ++
+    } while (num <= 7)
+}
