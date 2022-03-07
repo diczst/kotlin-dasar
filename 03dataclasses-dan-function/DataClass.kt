286bc28d078ee73d8c6beb84718a965104add44c
dzuerst <dzuerst@gmail.com>
2022-03-07 13:43:50 +0700

    dataclass

---
+++ 03dataclasses-dan-function/DataClass.kt
@@ -0,0 +1,11 @@
+// Perbedaan class biasa dan data class
+
+class User(val name: String, val age: Int)
+
+// sintaks datas data class
+// cukup tambahkan keyword data sebelum class
+data class DataUser(val name: String, val age: Int)
+
+fun main {
+
+}
